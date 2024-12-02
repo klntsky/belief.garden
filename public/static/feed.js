a385ebc65599cdefb29ee4ec84e4e555cfc45607
Vladimir Kalnitsky <klntsky@gmail.com>
2024-12-03 03:39:03 +0400

    Improve feed format for `new_reply`

--- public/static/feed.js
+++ public/static/feed.js
@@ -109,9 +109,8 @@ function getActionElements(entry) {
       container.appendChild(document.createTextNode(' replied to a comment on '));
       const profileLink = createUserLink(entry.profileName);
       container.appendChild(profileLink);
-      container.appendChild(document.createTextNode('\'s belief "'));
+      container.appendChild(document.createTextNode('\'s belief '));
       container.appendChild(createBeliefLink(entry.profileName, entry.beliefName));
-      container.appendChild(document.createTextNode('"'));
       break;
     }
 
